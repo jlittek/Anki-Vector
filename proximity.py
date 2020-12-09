46017e54b397569247b74da0a0b375d92552385a
jlittek <75247695+jlittek@users.noreply.github.com>
2020-12-09 16:04:09 +0100

    stoppt vor Objekt

--- proximity.py
+++ proximity.py
@@ -1,7 +1,12 @@
 import anki_vector
+from anki_vector.util import distance_mm, Distance, speed_mmps
 
 with anki_vector.Robot() as robot:
+	robot.motors.set_wheel_motors(50,50)
 	while True:
-	    proximity_data = robot.proximity.last_sensor_reading
-	    if proximity_data is not None:
-	        print('Proximity distance: {0}'.format(proximity_data.distance))
+		proximity_data = robot.proximity.last_sensor_reading
+		print('Proximity distance: {0}'.format(proximity_data.distance))
+		if proximity_data.distance.distance_mm < 40:
+			robot.motors.stop_all_motors()
+			
+	
